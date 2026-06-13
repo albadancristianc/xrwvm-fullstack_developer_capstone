61719cddfa46b85e07b4a35026b62f786a1da667
Your Name <you@example.com>
2026-06-12 23:48:57 -0400

    fix linting errors

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,11 +1,7 @@
-# Uncomment the following imports before adding the Model code
-
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
-# Create your models here.
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
@@ -21,7 +17,6 @@ class CarModel(models.Model):
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
         ('WAGON', 'Wagon'),
-        # Add more choices as required
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(
@@ -32,13 +27,3 @@ class CarModel(models.Model):
 
     def __str__(self):
         return self.name
-
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,9 +30,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://albadancrist-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+    'https://albadancrist-8000.theiadockernext-1-labs-prod-theiak8s-4'
+    '-tor01.proxy.cognitiveclass.ai'
+]
 CSRF_TRUSTED_ORIGINS = [
-    'https://albadancrist-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+    'https://albadancrist-8000.theiadockernext-1-labs-prod-theiak8s-4'
+    '-tor01.proxy.cognitiveclass.ai'
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -97,7 +101,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    'django.contrib.auth.password_validation'
+    '.UserAttributeSimilarityValidator',
     },
     {
         'NAME':
